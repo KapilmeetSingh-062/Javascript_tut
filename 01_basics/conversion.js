67388c26ee984802514161a26977d1c06941de24
KapilmeetSingh-062 <72926497+KapilmeetSingh-062@users.noreply.github.com>
2023-12-12 00:51:06 +0000

    learning js

---
+++ 01_basics/conversion.js
@@ -0,0 +1,20 @@
+let score = 33
+let string = "33"
+
+console.log(typeof score)
+console.log(typeof string)
+
+let toNum = Number((string))
+console.log(typeof toNum)
+
+let isLogged = 1
+
+let booleanIsLogged = Boolean(isLogged)
+
+console.log(booleanIsLogged)
+
+const str = `
+hi this
+
+`;
+console.log(str.trim());
---
+++ 01_basics/object.js
@@ -0,0 +1,5 @@
+const myObj = {1 : "haha", 1 : "hehe", 2 : "haha2", 1: "jeje", name: "ty", name: "bye"}
+
+console.log(myObj[1]);
+console.log(myObj[2]);
+console.log(myObj.name);
